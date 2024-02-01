ff562a966fa22ef1f14384106e4e66c6d68b5104
Daniel Mizuki <dmizuki12@gmail.com>
2024-01-31 16:08:12 -0800

    Changed return message

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -69,4 +69,4 @@ def test_lambda_handler(apigw_event):
 
     assert ret["statusCode"] == 200
     assert "message" in ret["body"]
-    assert data["message"] == "hello world my friend"
+    assert data["message"] == "hello world! I'm using canary deployments"
